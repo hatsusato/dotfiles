cade52a2b0844a61178ce470cc18e35f236895d0
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-05-02 00:46:30 +0900

    Merge expand.sh into patch.sh

--- patch.sh
+++ patch.sh
@@ -2,6 +2,28 @@
 
 set -eu
 
-patch=${1#/}.patch
-./expand.sh "$patch" | patch --dry-run -f -p0 -R -s "$1" >/dev/null && exit
-./expand.sh "$patch" | sudo patch -b -f -p0 -Vt "$1"
+expand() {
+    local script= var
+    while read var; do
+        if [[ -n $var && -n ${!var+x} ]]; then
+            script+="s/\${$var}/${!var}/g;"
+        else
+            echo "$0: \${$var}: unbound variable" >&2
+            exit 1
+        fi
+    done < <(grep -o '${[A-Z_]*}' "$1" | tr -d '${}' | sort -u)
+    if [[ -n $script ]]; then
+        sed -e "$script" "$1"
+    else
+        cat "$1"
+    fi
+}
+
+patch=${1#$HOME}
+patch=${patch#/}.patch
+expand "$patch" | patch --dry-run -f -p0 -R -s "$1" >/dev/null && exit
+if [[ -w "$1" ]]; then
+    patch -b -f -p0 -Vt "$1"
+else
+    sudo patch -b -f -p0 -Vt "$1"
+fi < <(expand "$patch")
