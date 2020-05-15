d2278f7b4c13b1ac8b1f147aa81470fc9167d8af
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-05-15 17:36:31 +0900

    Add yubikey.sh

---
+++ bin/yubikey.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -eu
+
+yubikey-aid() {
+    local pattern='match($1, /^Reader$/) {print $4}'
+    AID=$(gpg --card-status --with-colons 2>/dev/null | awk -F: "$pattern")
+    [[ -n $AID ]]
+}
+yubikey-id() {
+    local line IFS=:
+    while read line; do
+        set -- $line
+        case $1 in
+            sec) KEYID=$5;;
+            ssb) [[ ${15} == $AID ]] && return;;
+        esac
+    done < <(gpg --list-secret-keys --with-colons)
+    KEYID=
+    return 1
+}
+yubikey-init() {
+    mkdir -m700 -p /tmp/$USER
+    export GNUPGHOME=/tmp/$USER/.gnupg
+    until yubikey-aid; do
+        read -n1 -p 'Insert YubiKey and Hit Any Key.' -s
+        echo
+    done >&2
+    until yubikey-id; do
+        gpg --batch --command-fd 0 --card-edit <<<$'fetch\nquit\n'
+    done >&2 2> >(grep ^gpg:)
+}
