27cc32479ee916aa3bc969e8ae51a27caa169536
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-05-16 03:52:04 +0900

    Change indent width

--- bin/tmp-user.sh
+++ bin/tmp-user.sh
@@ -3,8 +3,8 @@
 set -eu
 
 tmp-user() {
-    local dir=/tmp/$USER
-    mkdir -m700 -p "$dir"
-    (($#)) && dir+=/$1
-    echo "$dir"
+  local dir=/tmp/$USER
+  mkdir -m700 -p "$dir"
+  (($#)) && dir+=/$1
+  echo "$dir"
 }
--- bin/yubikey.sh
+++ bin/yubikey.sh
@@ -5,29 +5,29 @@ set -eu
 source "${BASH_SOURCE%/*}"/tmp-user.sh
 
 yubikey-aid() {
-    local pattern='match($1, /^Reader$/) {print $4}'
-    AID=$(gpg --card-status --with-colons 2>/dev/null | awk -F: "$pattern")
-    [[ -n $AID ]]
+  local pattern='match($1, /^Reader$/) {print $4}'
+  AID=$(gpg --card-status --with-colons 2>/dev/null | awk -F: "$pattern")
+  [[ -n $AID ]]
 }
 yubikey-id() {
-    local line IFS=:
-    while read line; do
-        set -- $line
-        case $1 in
-            sec) KEYID=$5;;
-            ssb) [[ ${15} == $AID ]] && return;;
-        esac
-    done < <(gpg --list-secret-keys --with-colons)
-    KEYID=
-    return 1
+  local line IFS=:
+  while read line; do
+    set -- $line
+    case $1 in
+      sec) KEYID=$5;;
+      ssb) [[ ${15} == $AID ]] && return;;
+    esac
+  done < <(gpg --list-secret-keys --with-colons)
+  KEYID=
+  return 1
 }
 yubikey-init() {
-    export GNUPGHOME=$(tmp-user .gnupg)
-    until yubikey-aid; do
-        read -n1 -p 'Insert YubiKey and Hit Any Key.' -s
-        echo
-    done >&2
-    until yubikey-id; do
-        gpg --batch --command-fd 0 --card-edit <<<$'fetch\nquit\n'
-    done >&2 2> >(grep ^gpg:)
+  export GNUPGHOME=$(tmp-user .gnupg)
+  until yubikey-aid; do
+    read -n1 -p 'Insert YubiKey and Hit Any Key.' -s
+    echo
+  done >&2
+  until yubikey-id; do
+    gpg --batch --command-fd 0 --card-edit <<<$'fetch\nquit\n'
+  done >&2 2> >(grep ^gpg:)
 }
