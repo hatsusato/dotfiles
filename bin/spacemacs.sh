e6ce2c78cbd55c052e2a3896db9909f8009461bb
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-05-21 17:15:28 +0900

    Show message when spacemacs-daemon hung up

--- bin/spacemacs.sh
+++ bin/spacemacs.sh
@@ -7,5 +7,6 @@ resolve-hup() {
   local err=0
   spacemacs-exists || err=$?
   ((err == 124)) || return 0
+  notify-send -u critical -i emacs spacemacs-daemon 'HUNG-UP DETECTED!'
   pkill -9 -f 'spacemacs --daemon' || return 0
 }
