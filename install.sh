2d1205ca87810d9499888d72a7fed7dfe4795adb
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-05-20 14:06:13 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,26 +2,38 @@
 
 set -eu
 
+readonly TOP_DIR=${BASH_SOURCE%/*}
+
+apt-comment() {
+  grep '^#' "$src" | grep -m1 'apt:' | cut -d: -f2
+}
 apt-install() {
-  (($#)) || return 0
-  local -i n=$(dpkg --no-pager -l "$@" 2>/dev/null | grep ^ii | wc -l)
-  (($# == n)) || sudo apt install "$@"
+  local -a apt=("$TOP_DIR"/.local/bin/apt-install)
+  apt+=($(apt-comment))
+  "${apt[@]}"
+}
+backup() {
+  local patch=$TOP_DIR/${src##*/}.patch
+  local -a diff=(diff -u)
+  diff+=(--label "$src")
+  diff+=(--label "$src")
+  diff+=("$src" "$dst")
+  "${diff[@]}" &>/dev/null && return
+  echo "generate patch: ${patch@Q}"
+  "${diff[@]}" >"$patch" || :
 }
 copy() {
-  local -i mode=555
-  [[ $src == *.sh ]] && mode=444
-  [[ $src -ot $dst ]] && cp -afTv "$dst" "$src".bak
-  install -C -D -m $mode -v -T "$src" "$dst"
+  local -i mode=444
+  [[ -x $src ]] && mode=555
+  LANG=C install -C -D -m $mode -v -T "$src" "$dst"
 }
 main() {
   local src dst
   for src; do
     [[ -f $src ]] || continue
-    apt-install $(grep -m1 '^# apt:' "$src" | sed 's/.*://')
-    dst=$HOME/.local/$src
-    if [[ -f $dst ]]; then
-      diff -q "$src" "$dst" &>/dev/null && continue
-    fi
+    apt-install
+    dst=$HOME/$src
+    [[ -f $dst ]] && backup
     copy
   done
 }
