2db6e5f9505364f639a39976bc6605014a605699
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-05-27 22:22:58 +0900

    Update install-chrome.sh

--- install-chrome.sh
+++ install-chrome.sh
@@ -6,11 +6,20 @@ download() {
   mkdir -m700 -p "${file%/*}"
   wget --no-verbose --show-progress -O "$file" "$url" || exit
 }
+show-dpkg() {
+  dpkg --no-pager -l "$@" 2>/dev/null | tail -n+6
+}
 is-installed() {
-  dpkg --no-pager -l $pkg 2>/dev/null | grep -q ^ii
+  show-dpkg $pkg | grep -q ^ii
 }
 apt-install() {
   sudo apt-get install -qq "$file"
+}
+append() {
+  local log=$HOME/.config/local/.install
+  [[ -f $log ]] || error file not found: "$log"
+  grep -xq $pkg "$log" && return
+  tee -a "$log" <<<"$pkg" >/dev/null
 }
 main() {
   local pkg deb url file
@@ -20,6 +29,7 @@ main() {
   file=/usr/local/src/$USER/$deb
   [[ -f $file ]] || download
   is-installed || apt-install
+  append
 }
 
 main
