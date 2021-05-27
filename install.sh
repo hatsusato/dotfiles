14a50468f5a3b5b1f74c2ab65a9e46c60661b245
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-05-27 14:05:37 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,12 +5,12 @@ set -eu
 readonly TOP_DIR=${BASH_SOURCE%/*}
 
 apt-comment() {
-  grep '^#' "$src" | grep -m1 'apt:' | cut -d: -f2
+  grep '^#' "$src" | grep -m1 'apt:' | cut -d: -f2-
 }
 apt-install() {
   local -a apt=("$TOP_DIR"/.local/bin/apt-wrapper install)
   apt+=($(apt-comment))
-  export APT_OPTION=--yes
+  export APT_OPTION=-qq
   "${apt[@]}"
 }
 backup() {
@@ -27,7 +27,7 @@ backup() {
 copy() {
   local -i mode=444
   [[ -x $src ]] && mode=555
-  LANG=C install -C -D -m $mode -v -T "$src" "$dst"
+  LANG=C install -D -m $mode -v -T "$src" "$dst"
 }
 main() {
   local src dst
