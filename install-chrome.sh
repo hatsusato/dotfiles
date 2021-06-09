431d66f52e6d14b2962d1b1770ca7a6b0a357bea
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-06-09 18:49:10 +0900

    Fix install-chrome.sh

--- install-chrome.sh
+++ install-chrome.sh
@@ -2,17 +2,33 @@
 
 set -eu
 
+setup-dir() {
+  sudo mkdir -p "$dir"
+  sudo chown $USER:$USER "$dir"
+}
 download() {
-  mkdir -m700 -p "${file%/*}"
   wget --no-verbose --show-progress -O "$file" "$url" || exit
 }
+install-chrome() {
+  dpkg -l "$pkg" 2>/dev/null | grep ^ii && exit
+  sudo apt-get install -qq "$file" || exit failed to install
+}
+append() {
+  local log=$HOME/.config/local/.install
+  [[ -f $log ]] || exit file not found: "$log"
+  grep -xq "$pkg" "$log" &>/dev/null && return
+}
 main() {
-  local deb url file
-  deb=google-chrome-stable_current_amd64.deb
+  local pkg deb url dir file
+  pkg=google-chrome-stable
+  deb=${pkg}_current_amd64.deb
   url=https://dl.google.com/linux/direct/$deb
-  file=/usr/local/src/$USER/$deb
+  dir=/usr/local/src/$USER
+  file=$dir/$deb
+  [[ -d $dir && -w $dir ]] || setup-dir
   [[ -f $file ]] || download
-  ./install-apt.sh "$file"
+  install-chrome
+  append
 }
 
 main
