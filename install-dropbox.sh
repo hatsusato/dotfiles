6256eb9cbfae598a13374925e3ee7903576decb9
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-06-09 18:25:41 +0900

    Update install-dropbox.sh

--- install-dropbox.sh
+++ install-dropbox.sh
@@ -2,7 +2,12 @@
 
 set -eu
 
+install-dropbox() {
+  dpkg -l nautilus-dropbox 2>/dev/null | grep -q ^ii && return
+  sudo apt-get install -qq nautilus-dropbox
+}
 main() {
+  install-dropbox
   dropbox start -i
   dropbox status
   dropbox status | grep -Fqx '最新の状態'
