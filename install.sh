da5fb91e3b721871474721150181fbadd1de8b47
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-06-09 18:47:01 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,35 +3,26 @@
 set -eu
 source "${BASH_SOURCE%/*}"/error.sh
 
-backup() {
-  local patch=${src##*/}.patch
-  local -a diff=(diff -u)
-  diff+=(--label "$src")
-  diff+=(--label "$src")
-  diff+=("$src" "$dst")
-  [[ $src -nt $dst ]] && return
-  "${diff[@]}" &>/dev/null && return
-  echo "generate patch: ${patch@Q}"
-  "${diff[@]}" >"$patch" || :
-}
 copy() {
-  local -a sudo=(sudo) install=(install -D -v -T)
+  local sudo=sudo
+  local -a install=(install -D -v -T)
   if [[ $dst == $HOME/* ]]; then
-    sudo=()
+    sudo=
     if [[ -x $src ]]; then
       install+=(-m 555)
     else
       install+=(-m 444)
     fi
   fi
-  LANG=C ${sudo[@]} "${install[@]}" "$src" "$dst"
+  LANG=C $sudo "${install[@]}" "$src" "$dst"
 }
 main() {
-  (($# == 2)) || return
-  local src=$1 dst=$2
+  local src dst
+  (($# == 2)) || exit
+  src=$1
+  dst=$2
   [[ $dst == /* ]] || error not full path: "$dst"
   [[ -f $src ]] || error source not found: "$src"
-  [[ -f $dst ]] && backup
   copy
 }
 
