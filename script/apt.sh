828209ebd76e81f7fbaa083c73d9aa0d40c4b270
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-08-07 18:06:47 +0900

    Fix apt.sh

--- script/apt.sh
+++ script/apt.sh
@@ -6,31 +6,16 @@ source "${BASH_SOURCE%/*}"/error
 is-installed() {
   dpkg -l "$pkg" 2>/dev/null | tail -n+6 | grep -q ^ii
 }
-filter-pkgs() {
+main() {
   local pkg
-  pkgs=()
+  local -a pkgs=()
   for pkg; do
     is-installed "$pkg" && continue
     pkgs+=("$pkg")
   done
-}
-register-pkgs() {
-  local pkg
-  for pkg; do
-    grep -qx "$pkg" "$log" && continue
-    tee -a "$log" <<<"$pkg" >/dev/null
-  done
-}
-main() {
-  local log=$HOME/.config/local/.install
-  local -a pkgs
-  mkdir -p "${log%/*}"
-  touch "$log"
-  [[ -w $log ]] || error "file not writable: $log"
-  filter-pkgs "${pkgs[@]}"
   ((${#pkgs[@]} == 0)) && return
   sudo apt-get -y install "${pkgs[@]}"
-  register-pkgs "${pkgs[@]}"
+  "${BASH_SOURCE%/*}"/apt-register.sh "${pkgs[@]}"
 }
 
 main "$@"
