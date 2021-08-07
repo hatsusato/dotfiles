387bb83a1ac4830d9dc09fc65e95887ac13ca520
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-08-07 22:29:23 +0900

    Update spacemacs.sh

--- script/spacemacs.sh
+++ script/spacemacs.sh
@@ -7,6 +7,7 @@ main() {
   local url=https://github.com/syl20bnr/spacemacs
   [[ -d $target/.git ]] && return
   git clone --branch develop "$url" "$target"
+  "${BASH_SOURCE%/*}"/apt.sh emacs emacs-mozc
   [[ -f $HOME/.spacemacs ]] || emacs
 }
 
