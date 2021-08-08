c436c51cc339426945798455333fe0fff2c9a051
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-08-08 20:34:58 +0900

    Update install.sh

--- script/install.sh
+++ script/install.sh
@@ -4,25 +4,22 @@ set -eu
 source "${BASH_SOURCE%/*}"/error
 
 copy() {
-  local sudo=sudo
   local -a install=(install -D -v -T)
-  if [[ $dst == $HOME/* ]]; then
-    sudo=
-    if [[ -x $src ]]; then
-      install+=(-m 555)
-    else
-      install+=(-m 444)
-    fi
+  if [[ -x $src ]]; then
+    install+=(-m 555)
+  else
+    install+=(-m 444)
   fi
   LANG=C $sudo "${install[@]}" "$src" "$dst"
 }
 main() {
-  local src dst
+  local src dst sudo=sudo
   (($# == 2)) || exit
   src=$1
   dst=$2
-  [[ $dst == /* ]] || error not full path: "$dst"
   [[ -f $src ]] || error source not found: "$src"
+  [[ $dst == /* ]] || error not full path: "$dst"
+  [[ $dst == $HOME/* ]] && sudo=
   copy
 }
 
