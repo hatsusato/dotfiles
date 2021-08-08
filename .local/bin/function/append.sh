302df639038ed32c8d2b8757f0c408eac0b0d4f5
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-08-08 20:28:21 +0900

    Move append.sh

---
+++ .local/bin/function/append.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -eu
+source "${BASH_SOURCE%/*}"/error
+
+join() {
+  tr '\n' '\0' <"$1"
+}
+contains() {
+  [[ -f $dst ]] || return
+  grep -Fqz -f <(join "$src") <(join "$dst")
+}
+main() {
+  local src dst sudo=sudo
+  (($# == 2)) || exit
+  src=$1
+  dst=$2
+  [[ -f $src ]] || error file not found: "$src"
+  [[ -d ${dst%/*} ]] || error directory not found: "${dst%/*}"
+  contains && exit
+  [[ $dst == $HOME/* ]] && sudo=
+  $sudo tee -a "$dst" <"$src" >/dev/null
+}
+
+main "$@"
