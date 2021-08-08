e349e904c89167daea2c419f0b5dd73934efd057
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-08-08 22:06:21 +0900

    Rename apt-is-installed

--- .local/bin/function/apt.sh
+++ .local/bin/function/apt.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 set -eu
-source "${BASH_SOURCE%/*}"/apt-is-installed
+source "${BASH_SOURCE%/*}"/apt-installed-all
 source "${BASH_SOURCE%/*}"/apt-register
 
 main() {
   local pkg
   local -a pkgs=()
   for pkg; do
-    apt-is-installed "$pkg" || pkgs+=("$pkg")
+    apt-installed-all "$pkg" || pkgs+=("$pkg")
   done
   ((${#pkgs[@]} == 0)) && return
   sudo apt-get -y install "${pkgs[@]}"
--- script/chrome-install.sh
+++ script/chrome-install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 set -eu
-source "${BASH_SOURCE%/*}"/function/apt-is-installed
+source "${BASH_SOURCE%/*}"/function/apt-installed-all
 source "${BASH_SOURCE%/*}"/function/apt-register
 source "${BASH_SOURCE%/*}"/function/error
 
 main() {
   local file=$1 pkg=google-chrome-stable
-  apt-is-installed "$pkg" && exit
+  apt-installed-all "$pkg" && exit
   sudo apt-get -qq install "$file" || error failed to install
   apt-register "$pkg"
 }
